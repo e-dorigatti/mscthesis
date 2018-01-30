17614cb4e0dcb5ad368817d8ce3f091da98bd483
e-dorigatti <emilio.dorigatti@gmail.com>
2018-01-30 10:47:00 +0100

    added plan

---
+++ plan/auto/plan.el
@@ -0,0 +1,46 @@
+(TeX-add-style-hook
+ "plan"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:org2dc2040"
+    "sec:org25b2646"
+    "sec:org24bf353"
+    "sec:org17e84f8"
+    "sec:orgc3c4ae9"
+    "sec:orgaf17169"
+    "sec:org66d6079"
+    "sec:orga3c0cbe"
+    "sec:org665a0ea"
+    "sec:org9b365ad"
+    "sec:orge8aaada"
+    "sec:org9b5d6c7"))
+ :latex)
+
